d553c2777b7b1b6548c62d90ab6a4358696dca26
silvana <silvanaamaralpe@gmail.com>
2021-05-21 00:08:51 -0300

    resposta da pergunta 5

---
+++ semana02/atividades02/resposta5.js
@@ -0,0 +1,6 @@
+//Gosto de questoes assim , clássicas . A média é calculada adicionando um grupo de números e dividindo pela a contagem desses números. 
+// atribui valor a três notas e tirei a media , somando os valores atribuidos e dividindo pelo o numero de notas que é 3 .
+
+let media = (20 + 60 + 80)/3;
+
+console.log(media);
