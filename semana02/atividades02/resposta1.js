ee95dc43e2e04e4dbeaf097bdae5925e845a0734
silvana <silvanaamaralpe@gmail.com>
2021-05-21 00:06:21 -0300

    resposta da pergunta 1

---
+++ semana02/atividades02/resposta1.js
@@ -0,0 +1,10 @@
+// Primeiramente declarei as variáveis , armei a questão do problema , montando os calculos , fiz em pequenos passos .
+
+let valormercadoria = 4000;
+const percentualdesconto = 60;
+
+let valorcomdesconto = valormercadoria * (percentualdesconto/100); 
+
+let precoapagar = valormercadoria - valorcomdesconto;
+
+console.log(valorcomdesconto , precoapagar);
