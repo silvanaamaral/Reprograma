e6818f2b84f4cdb74d2ec505054d79dfbc5f7b4d
silvana <silvanaamaralpe@gmail.com>
2021-05-21 00:07:25 -0300

    resposta da pergunta 2

---
+++ semana02/atividades02/resposta2.js
@@ -0,0 +1,10 @@
+//Primeiramente declarei as variáveis , em seguida fiz o calculo , gostei de fazer dessa forma .
+
+let distancia = 300;
+let velocidademedia = 100;
+
+let tempo = distancia / velocidademedia;
+
+console.log(tempo + " horas");
+
+
