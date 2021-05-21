9a25f480d786920f05a4d9ec4d4b149510d5be3a
silvana <silvanaamaralpe@gmail.com>
2021-05-21 00:08:32 -0300

    resposta da pergunta 4

---
+++ semana02/atividades02/resposta4.js
@@ -0,0 +1,10 @@
+//Bem , aqui eu atribui valores as variáveis . Usei o Const (aí o valor não se altera ) , let (com possibilidade de alteração)
+// fiz passo a passo , conforme o problema pedia ,  cálculo  simples e direto.
+const valordocarrodia = 60.00;
+const valorkmrodado = 0.15;
+let quantidadedias = 30;
+let quantidadekm = 600;
+
+let  precoapagar = valordocarrodia * quantidadedias + valorkmrodado * quantidadekm;
+
+console.log(precoapagar);
