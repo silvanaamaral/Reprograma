acc8884865879270eb140b246a913ab104bfa18e
silvana <silvanaamaralpe@gmail.com>
2021-05-21 00:10:07 -0300

    resposta da pergunta 7

---
+++ semana02/atividades02/resposta7.js
@@ -0,0 +1,20 @@
+//Trabalhei com a estrutura condicional If e Else testando as instruções , caso a condição seja VERDADEIRA  o bloco será executado,
+// se FALSO , ele executará o próximo comando . Desse modo , executará os comandos conforme os resultados .
+
+let entrada1 = 10;
+let entrada2 = 30 ;
+
+if (entrada1 === entrada2 ){
+
+    console.log("os numeros sao iguais");
+}
+else if(entrada1 > entrada2 ){
+
+     console.log(entrada1 + " > " + entrada2);
+}
+else if (entrada1 < entrada2){
+    
+    console.log(entrada1 + " < " + entrada2);
+}
+    
+
