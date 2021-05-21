cd75d4a72de84e5b95238f304a564965162730f2
silvana <silvanaamaralpe@gmail.com>
2021-05-21 00:09:29 -0300

    resposta da pergunta 6

---
+++ semana02/atividades02/resposta6.js
@@ -0,0 +1,32 @@
+// Aqui eu usei os comando de decisão switch , para selecionar uma das condições de entrada . Você digita a entrada e aparecerá o numero correspondente .
+
+
+let entrada = "segunda";
+
+switch(entrada){
+    case "segunda":
+        console.log(1);
+        break
+    case "terça":
+        console.log(2);
+        break
+    case "quarta":   
+        console.log(3);
+        break
+    case "quinta":
+        console.log(4);
+        break
+    case "sexta":
+        console.log(5);
+        break
+    case "sábado":
+        console.log(6);
+        break
+    case "domingo":
+        console.log(7);
+        break
+    default:
+        console.log("não é dia de semana");     
+
+
+}
