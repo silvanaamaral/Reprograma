611800768dabd8aa4d82fffc73002a8cc9becbc4
silvanaamaralpe@gmail.com <amaral>
2021-05-28 22:54:01 -0300

    resposta da atividade4

---
+++ semana03/atividade03/exercicio4.js
@@ -0,0 +1,22 @@
+
+
+
+
+for (let num = 1; num <=100; num++){
+    console.log(num)
+}
+    
+for (let num =1; num <=100; num++){
+    if(num === 51){
+        break
+    }
+    console.log(num)
+}
+    
+for (let num = 1; num <=100; num++){
+    if(num === 50){
+        continue
+    }
+    console.log(num)
+} 
+    
