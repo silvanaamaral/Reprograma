9efb6e4467e8086c5bd03dcd252a2aa624bcb628
silvanaamaralpe@gmail.com <amaral>
2021-05-28 22:50:48 -0300

    resposta da atividade3

---
+++ semana03/atividade03/exercicio3.js
@@ -0,0 +1,15 @@
+
+
+
+
+function divisao(num1 , num2){
+    if(num1%num2 === 0){
+        console.log(`o resultado é par`)
+        return num1/num2
+    }else if (num1%num2){
+        console.log
+        return num1/num2
+    }
+    
+}
+ console.log(`o resultado é : `+ divisao(20,4))   
